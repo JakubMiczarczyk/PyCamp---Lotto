e1088096409aca8335978eaad68510104f609e25
dev global <milczarczykjakub@gmail.com>
2023-09-20 00:17:41 +0200

    Fix draw function to draw from wright_numbers

--- lotto.py
+++ lotto.py
@@ -1,35 +1,46 @@
-from random import Random
+from random import Random, sample
 
 print('Zagrajmy w LOTTO!\n\n'
       'Wybierz 6 liczb z przedziau od 1 do 49.\n'
       'Zakład kosztuje 3 zł, ciekawe ile wydasz aby wygrać?')
 
-wright_numbers = {1: 49}
+wright_numbers = set(range(1, 50))
 
 players_type = True
 
 player_numbers = set()
+
+# draw_set = set()
 
 
 def player_input(number):
     player_numbers.add(number)
 
 
-while players_type is True:
-    print('\nPodaj typowane liczby: ')
-
-    player_input(int(input('\nliczba 1 z 6: ')))
-    player_input(int(input('liczba 2 z 6: ')))
-    player_input(int(input('liczba 3 z 6: ')))
-    player_input(int(input('liczba 4 z 6: ')))
-    player_input(int(input('liczba 5 z 6: ')))
-    player_input(int(input('liczba 6 z 6: ')))
-
-    if player_numbers.issuperset(wright_numbers):
-        players_type = False
+def check_numbers(numbers):
+    if numbers.issubset(wright_numbers):
         print('\nPodano dobre liczby.\n'
+              f'{player_numbers}\n'
               'Sprawdźmy je!')
     else:
-        players_type = True
         print('\nPodano złe liczby.\n'
+              f'{player_numbers}\n'
               'Podaj 6 liczb od 1 do 49 Głąbie!\n')
+    return bool
+
+
+def draw():
+    draw_set = set(sample([wright_numbers], k=6))  # TODO
+    return draw_set
+
+
+print('\nPoniżej podaj typowane liczby: ')
+
+while players_type is True:
+    if len(player_numbers) < 6:
+        player_input(int(input('\nTypowana liczba: ')))
+        players_type = True
+    else:
+        players_type = check_numbers(player_numbers)
+
+print(f'Losowanie: {draw()}')
